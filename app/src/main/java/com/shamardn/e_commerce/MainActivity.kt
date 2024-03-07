7269ccd7ecb3b9fa05a252e167cb14ba28464da6
SHAMARDN <shamardn@gmail.com>
2024-03-07 21:40:23 +0200

    add animation to splash screen 12

--- app/src/main/java/com/shamardn/e_commerce/MainActivity.kt
+++ app/src/main/java/com/shamardn/e_commerce/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.shamardn.e_commerce
 
+import android.animation.ObjectAnimator
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
+import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
@@ -15,8 +19,27 @@ class MainActivity : AppCompatActivity() {
     private fun initSplashScreen() {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S){
             installSplashScreen()
+            // Add a callback that's called when the splash screen is animating to the
+            // app content.
+            splashScreen.setOnExitAnimationListener { splashScreenView ->
+                // Create your custom animation.
+                val slideUp = ObjectAnimator.ofFloat(
+                    splashScreenView,
+                    View.TRANSLATION_Y,
+                    0f,
+                    -splashScreenView.height.toFloat()
+                )
+                slideUp.interpolator = AnticipateInterpolator()
+                slideUp.duration = 2000L
+
+                // Call SplashScreenView.remove at the end of your custom animation.
+                slideUp.doOnEnd { splashScreenView.remove() }
+
+                // Run your animation.
+                slideUp.start()
+            }
         }else{
-            setTheme(R.style.Theme_MainActivity)
+            setTheme(R.style.Theme_E_Commerce)
         }
     }
 }
